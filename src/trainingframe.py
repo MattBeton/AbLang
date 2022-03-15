ac634111350dffb0fbedd88a767cbe7405695237
Tobias Olsen <olsen@iraqbabbler.stats.ox.ac.uk>
2022-03-15 14:34:22 +0000

    remove memory leak

--- src/trainingframe.py
+++ src/trainingframe.py
@@ -40,6 +40,8 @@ class TrainingFrame(pl.LightningModule):
 
         loss = self.loss_fn(output.view(-1, self.hparams.vocab_size), labels)
         
+        print(self.global_step)
+        
         # Must clear cache at regular interval
         if self.global_step % 10 == 0:
             torch.cuda.empty_cache()
@@ -78,7 +80,11 @@ class TrainingFrame(pl.LightningModule):
     
     def validation_epoch_end(self, val_step_outputs): # Updated once when validation is called
         
-        self.Evaluations(self, val_step_outputs)
+        
+        log_valuation_loss(self, val_step_outputs)
+        log_restoring_sequence(self)
+        
+        self.Evaluations(self)
 
     
     def configure_optimizers(self):
@@ -116,3 +122,60 @@ class TrainingFrame(pl.LightningModule):
 
 
 
+    
+    
+def log_valuation_loss(self, val_step_outputs):
+
+
+    val_loss = torch.stack([x['val_loss'] for x in val_step_outputs]).mean()
+    fw1_loss = torch.stack([x['fw1_loss'] for x in val_step_outputs]).mean()
+    cdr1_loss = torch.stack([x['cdr1_loss'] for x in val_step_outputs]).mean()
+    fw2_loss = torch.stack([x['fw2_loss'] for x in val_step_outputs]).mean()
+    cdr2_loss = torch.stack([x['cdr2_loss'] for x in val_step_outputs]).mean()
+    fw3_loss = torch.stack([x['fw3_loss'] for x in val_step_outputs]).mean()
+    cdr3_loss = torch.stack([x['cdr3_loss'] for x in val_step_outputs]).mean()
+    fw4_loss = torch.stack([x['fw4_loss'] for x in val_step_outputs]).mean()
+
+    self.logger.experiment["evaluation/eval_loss"].log(val_loss)
+    self.logger.experiment["evaluation/fw1_loss"].log(fw1_loss)
+    self.logger.experiment["evaluation/cdr1_loss"].log(cdr1_loss)
+    self.logger.experiment["evaluation/fw2_loss"].log(fw2_loss)
+    self.logger.experiment["evaluation/cdr2_loss"].log(cdr2_loss)
+    self.logger.experiment["evaluation/fw3_loss"].log(fw3_loss)
+    self.logger.experiment["evaluation/cdr3_loss"].log(cdr3_loss)
+    self.logger.experiment["evaluation/fw4_loss"].log(fw4_loss)
+
+    
+def log_restoring_sequence(self):
+    testSeq = 'EVQLVESGPGLVQPGKSLRLSCVASGFTFSGYGMHWVRQAPGKGLEWIALIIYDESNKYYADSVKGRFTISRDNSKNTLYLQMSSLRAEDTAVFYCAKVKFYDPTAPNDYWGQGTLVTVSS|'
+    aaPreds, aaPreds50 = singleSeqValidation(self, self.tokenizer, testSeq=testSeq)       
+    self.logger.experiment['evaluation/heavy_reconstruct'].log(aaPreds[0])
+    self.logger.experiment['evaluation/heavy_reconstruct_50'].log(aaPreds50[0])
+
+    testSeq = '|DIVMTQTPSTLSASVGDRVTLTCKASQDISYLAWYQQKPGKAPKKLIYAASSLQSGVPSRFSGSGSGTDFTLTISSLQPEDFATYYCLQQNSNWTFGQGTKVDIK'
+    aaPreds, aaPreds50 = singleSeqValidation(self, self.tokenizer, testSeq=testSeq)       
+    self.logger.experiment['evaluation/light_reconstruct'].log(aaPreds[0])
+    self.logger.experiment['evaluation/light_reconstruct_50'].log(aaPreds50[0])
+    
+    testSeq = 'EVQLVESGPGLVQPGKSLRLSCVASGFTFSGYGMHWVRQAPGKGLEWIALIIYDESNKYYADSVKGRFTISRDNSKNTLYLQMSSLRAEDTAVFYCAKVKFYDPTAPNDYWGQGTLVTVSS|DIVMTQTPSTLSASVGDRVTLTCKASQDISYLAWYQQKPGKAPKKLIYAASSLQSGVPSRFSGSGSGTDFTLTISSLQPEDFATYYCLQQNSNWTFGQGTKVDIK'
+    aaPreds, aaPreds50 = singleSeqValidation(self, self.tokenizer, testSeq=testSeq)       
+    self.logger.experiment['evaluation/paired_reconstruct'].log(aaPreds[0])
+    self.logger.experiment['evaluation/paired_reconstruct_50'].log(aaPreds50[0])
+        
+        
+def singleSeqValidation(model, tokenizer, testSeq):
+    """
+    Small function used to visualize the training by showing how the reconstruction of a given sequence is improved over training.
+    """
+
+    tokenPreds = model(tokenizer([testSeq], pad=True, device=model.device))
+    
+    tokenMAX = torch.max(torch.nn.Softmax(dim=-1)(tokenPreds), -1)
+
+    aaPreds = tokenizer(tokenMAX[1], encode=False)
+
+    unkMatrix = torch.zeros(tokenMAX[0].shape, dtype=torch.long, device=model.device) + 21
+    
+    aaPreds50 = ['-'.join(tokenizer(torch.where(tokenMAX[0]<=.5, unkMatrix, tokenMAX[1]).detach(), encode=False)[0].split('<unk>'))]
+
+    return aaPreds, aaPreds50
