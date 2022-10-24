a47be9f863c391480a871d27afa624b191feb8b5
Tobias Olsen <olsen@iraqbabbler.stats.ox.ac.uk>
2022-10-24 12:50:00 +0100

    updated ablang-1 to esm-2

--- ablang_train/models/ablang_1.py
+++ ablang_train/models/ablang_1.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import List, Optional, Tuple
 
 import torch
+from torch import nn
 import torch.nn.functional as F
 
 from .encoderblock import TransformerEncoder
@@ -11,122 +12,177 @@ from .embedding import AbEmbeddings
 class AbLang(torch.nn.Module):
     """Pretraining model includes Abrep and the head model used for training."""
     
-    def __init__(self, hparams):
+    def __init__(
+        self,
+        vocab_size,
+        hidden_embed_size,
+        n_attn_heads,
+        n_encoder_blocks,
+        padding_tkn,
+        mask_tkn,
+        layer_norm_eps: float = 1e-05,
+        dropout: float = 0.0, 
+        use_tkn_dropout: bool = False,
+    ):
         super().__init__()
-        self.hparams = hparams
         
-        self.AbRep = AbRep(self.hparams)       
-        self.AbHead = AbHead(self.hparams, self.AbRep.AbEmbeddings.AAEmbeddings.weight)
+        self.AbRep = AbRep(
+            vocab_size,
+            hidden_embed_size,
+            n_attn_heads,
+            n_encoder_blocks,
+            padding_tkn,
+            mask_tkn,
+            layer_norm_eps,
+            dropout, 
+            use_tkn_dropout,
+        )       
+        self.AbHead = AbHead(
+            vocab_size,
+            hidden_embed_size,
+            self.AbRep.aa_embed_layer.weight,
+            layer_norm_eps,
+        )
         
-    def forward(self, x, attention_mask=None):
+    def forward(self, tokens, return_attn_weights=False, return_rep_layers=[]):
         
-        representations = self.AbRep(x, attention_mask)
+        representations = self.AbRep(tokens, return_attn_weights, return_rep_layers)
         
-        likelihoods = self.AbHead(representations.last_hidden_states)
+        if return_attn_weights:
+            return representations.attention_weights
         
-        return likelihoods
+        elif return_rep_layers != []:
+            return representations.many_hidden_states
+        
+        else:
+            likelihoods = self.AbHead(representations.last_hidden_states)
+            return likelihoods
     
     def get_aa_embeddings(self):
-        "This function is used to extract the trained aa_embeddings."
-        return self.AbRep.AbEmbeddings.aa_embeddings
+        "Extracts the trained aa_embeddings."
+        return self.AbRep.aa_embed_layer
 
     
 class AbRep(torch.nn.Module):
-    """This is the AbRep model."""
+    """
+    AbRep (antibody representations), takes the tokenized sequence and create hidden_embed (representations).
+    """
     
-    def __init__(self, hparams):
+    def __init__(
+        self, 
+        vocab_size,
+        hidden_embed_size,
+        n_attn_heads,
+        n_encoder_blocks,
+        padding_tkn,
+        mask_tkn,
+        layer_norm_eps: float = 1e-05,
+        dropout: float = 0.0, 
+        use_tkn_dropout: bool = False,
+    ):
         super().__init__()
-        self.pad_tkn = hparams.pad_tkn
+        self.padding_tkn = padding_tkn
+        self.mask_tkn = mask_tkn
+        self.use_tkn_dropout = use_tkn_dropout
         
-        self.AbEmbeddings = AbEmbeddings(hparams)    
-        self.EncoderBlocks = torch.nn.ModuleList([TransformerEncoder(hparams) for _ in range(hparams.num_encoder_blocks)])
-        self.LayerNorm = torch.nn.LayerNorm(hparams.representation_size, eps=hparams.layer_norm_eps)
+        self.aa_embed_layer = nn.Embedding(
+            vocab_size, 
+            hidden_embed_size, 
+            padding_idx=padding_tkn,
+        )   
+        self.encoder_blocks = nn.ModuleList(
+            [TransformerEncoder(
+                hidden_embed_size,
+                n_attn_heads,
+                attn_dropout = dropout,
+                layer_norm_eps = layer_norm_eps,
+            ) for _ in range(n_encoder_blocks)]
+        )
+        self.layer_norm_after_encoder_blocks = nn.LayerNorm(hidden_embed_size, eps=layer_norm_eps)
         
-    def additional():
+    def token_dropout(hidden_embed, tokens, padding_mask):
         
-        if return_contacts:
-            need_head_weights = True
-
+        hidden_embed.masked_fill_((tokens == self.mask_tkn).unsqueeze(-1), 0.0)
+        # x: B x T x C
+        mask_ratio_train = 0.15 * 0.8
+        src_lengths = (~padding_mask).sum(-1)
+        mask_ratio_observed = (tokens == self.mask_tkn).sum(-1).to(x.dtype) / src_lengths
+        hidden_embed = hidden_embed * (1 - mask_ratio_train) / (1 - mask_ratio_observed)[:, None, None]
+        
+        return hidden_embed
+        
+    def forward(self, 
+                tokens, 
+                return_attn_weights=False, 
+                return_rep_layers=[],
+               ):
+        
         assert tokens.ndim == 2
-        padding_mask = tokens.eq(self.padding_idx)  # B, T
+        padding_mask = tokens.eq(self.padding_tkn)
 
-        x = self.embed_scale * self.embed_tokens(tokens)
-
-        if self.token_dropout:
-            x.masked_fill_((tokens == self.mask_idx).unsqueeze(-1), 0.0)
-            # x: B x T x C
-            mask_ratio_train = 0.15 * 0.8
-            src_lengths = (~padding_mask).sum(-1)
-            mask_ratio_observed = (tokens == self.mask_idx).sum(-1).to(x.dtype) / src_lengths
-            x = x * (1 - mask_ratio_train) / (1 - mask_ratio_observed)[:, None, None]
-
-        if padding_mask is not None:
-            x = x * (1 - padding_mask.unsqueeze(-1).type_as(x))
-
-        repr_layers = set(repr_layers)
-        hidden_representations = {}
-        if 0 in repr_layers:
-            hidden_representations[0] = x
-
-        if need_head_weights:
-            attn_weights = []
+        hidden_embed = self.aa_embed_layer(tokens)
         
-    def forward(self, features, attention_mask=None, output_attentions=False, output_representations=False):
+        if self.use_tkn_dropout:
+            hidden_embed = token_dropout(hidden_embed, tokens, padding_mask)        
         
-        attention_mask = (features == self.pad_tkn) # Needs to be here for when you eval
-
-        representation = self.AbEmbeddings(features)
-        
-        all_representations = () if output_representations else None
-        all_self_attentions = () if output_attentions else None
-
-        
-        for EncoderBlock in self.EncoderBlocks:  
-            representation, attentions = EncoderBlock(representation, attention_mask, output_attentions)
+        return_rep_layers = set(return_rep_layers)
+        rep_layers = {}
+        if 0 in return_rep_layers:
+            rep_layers[0] = hidden_embed
             
-            if output_representations: 
-                all_representations = all_representations + (representation,) # Takes out each hidden states after each EncoderBlock
+        all_attn_weights = []
+        
+        for n_layer, encoder_block in enumerate(self.encoder_blocks):
+            hidden_embed, attn_weights = encoder_block(hidden_embed, padding_mask, return_attn_weights)
             
-            if output_attentions: 
-                all_self_attentions = all_self_attentions + (attentions,) # Takes out attention layers for analysis
+            if (n_layer + 1) in return_rep_layers: 
+                rep_layers[n_layer + 1] = hidden_embed
+            
+            if return_attn_weights: 
+                all_attn_weights.append(attn_weights)
            
-        representation = self.LayerNorm(representation)
+        hidden_embed = self.layer_norm_after_encoder_blocks(hidden_embed)
 
-        return AbRepOutput(last_hidden_states=representation, all_hidden_states=all_representations, attentions=all_self_attentions)
+        return DataAbRep(last_hidden_states=hidden_embed, many_hidden_states=rep_layers, attention_weights=all_attn_weights)
     
 
 class AbHead(torch.nn.Module):
-    """Head model for masked sequence prediction."""
+    """
+    AbHead (antibody head model), creates amino acid probabilities for each position based on the hidden_embed (representations).
+    """
     
-    def __init__(self, hparams, weights):
+    def __init__(
+        self, 
+        vocab_size,
+        hidden_embed_size,
+        weights,
+        layer_norm_eps: float = 1e-05,
+    ):
         super().__init__()
         
         self.ff = torch.nn.Sequential(
-            torch.nn.Linear(hparams.representation_size, hparams.representation_size),
-            torch.nn.GELU(),
-            torch.nn.LayerNorm(hparams.representation_size, eps=hparams.layer_norm_eps),
+            nn.Linear(hidden_embed_size, hidden_embed_size),
+            nn.GELU(),
+            nn.LayerNorm(hidden_embed_size, eps=layer_norm_eps),
         )
 
-        self.weight = weights
-        self.bias = torch.nn.Parameter(torch.zeros(hparams.vocab_size))   
-        self.final_layernorm = torch.nn.LayerNorm(hparams.vocab_size,
-                                                  eps=hparams.layer_norm_eps)
+        self.weights = weights
+        self.bias = nn.Parameter(torch.zeros(vocab_size))
 
-    def forward(self, features, **kwargs):
+    def forward(self, hidden_embed):
         
-        x = self.ff(features)
-
-        x = F.linear(x, self.weight) + self.bias
+        hidden_embed = self.ff(hidden_embed)
+        logits = F.linear(hidden_embed, self.weights) + self.bias
         
-        return self.final_layernorm(x)
+        return logits
 
     
 @dataclass
-class AbRepOutput():
+class DataAbRep():
     """
     Dataclass used to store AbRep output.
     """
 
     last_hidden_states: torch.FloatTensor
-    all_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
-    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    many_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attention_weights: Optional[Tuple[torch.FloatTensor]] = None
