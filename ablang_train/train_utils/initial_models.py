fe0dc9363a3d1e82a9ea057569a0b45be282bc3a
TobiasHeOl <tobiasheol@gmail.com>
2023-03-14 11:04:39 +0000

    update tools

--- ablang_train/train_utils/initial_models.py
+++ ablang_train/train_utils/initial_models.py
@@ -14,16 +14,15 @@ class AbLangPaired_v1(pl.LightningModule):
         parser.add_argument('--dropout', type=float, default=0.1)
         parser.add_argument('--use_tkn_dropout', type=strtobool, default=False)
         parser.add_argument('--loss_fn', type=str, default="CrossEntropy_Loss")
-
+        
         parser.add_argument('--mask_percent', type=float, default=.15, help='Percentage to mask.')
         parser.add_argument('--variable_masking', type=strtobool, default=False, help='Random uniform masking between 0 and mask_percent for each batch.')
         parser.add_argument('--mask_technique', type=str, default="random", help='masking technique to use. {standard, standard-sl_mask}')
         parser.add_argument('--change_percent', type=float, default=.1, help='Change percent.')
-        parser.add_argument('--leave_percent', type=float, default=.1, help='Leave percent.')        
+        parser.add_argument('--leave_percent', type=float, default=.1, help='Leave percent.')
         
         parser.add_argument('--initializer_range', type=float, default=0.02)
         parser.add_argument('--layer_norm_eps', type=float, default=1e-12)
-
 
         #max_grad_norm: 1
         #hparams.sync_batchnorm=True # Slower training, but might improve training
